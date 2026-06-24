1c958fa925c800bef19152fda4b431d75a3cccf9
defcon757 <aidanhernandez757@gmail.com>
2026-06-24 02:08:12 -0700

    Delete teensy-stompbox-synth directory

--- teensy-stompbox-synth/src/presets.h
+++
@@ -1,8 +0,0 @@
-// presets.h / presets.cpp
-// Save and load presets to microSD as simple CSV files
-// Preset 0 = PRESET_A.TXT, Preset 1 = PRESET_B.TXT, Preset 2 = PRESET_C.TXT
-
-#pragma once
-
-void loadPreset(int slot);
-void savePreset(int slot, int voiceIndex, int octave, float wetDry);
